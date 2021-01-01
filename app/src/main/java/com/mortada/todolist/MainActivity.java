2fcf63975440abc98b864c7ff74128b812382a1a
mortadamarouf <mortadamarouf@gmail.com>
2021-01-01 17:25:35 +0200

    Second commit

---
+++ app/src/main/java/com/mortada/todolist/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.application1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView textview2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textview2 = findViewById(R.id.textview2);
+
+    }
+
+    public void goToMain2(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/mortada/todolist/activity_login1.java
@@ -0,0 +1,67 @@
+package com.example.application1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+class login1 extends AppCompatActivity {
+    EditText emilUser,passwordUser;
+    Button login;
+    FirebaseAuth mAuth ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emilUser = findViewById(R.id.editTextTextEmailAddress2);
+        passwordUser= findViewById(R.id.editTextTextPassword2);
+        login = findViewById(R.id.button2);
+
+
+    }
+
+
+    public void doSignIn(View view) {
+
+        String email = emilUser.getText().toString();
+        String pasword = passwordUser.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, pasword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull final Task<AuthResult> task) {
+
+
+                        if (task.isSuccessful()) {
+
+                            Intent intent = new Intent(login1.this, listtodo.class);
+                            startActivity(intent);
+//
+                        } else {
+                            Toast.makeText(login1.this, "not login",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(login1.this, MainActivity2.class);
+                            startActivity(intent);
+                        }
+
+                    }
+                });
+
+    }
+}
